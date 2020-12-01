1f21897ed8773959ee8e3fbc3f4264767ae53b14
AndrewMMickel <andrew.m.mickel@gmail.com>
2020-12-01 14:05:31 -0800

    add multiple views and accompanying code

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -12,11 +12,24 @@ namespace FriendLetter.Controllers
     [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
 
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
     [Route("/")]
     public ActionResult Letter() {
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
       return View(myLetterVariable);
+    }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+    LetterVariable myLetterVariable = new LetterVariable();
+    myLetterVariable.Recipient = recipient;
+    myLetterVariable.Sender = sender;
+    return View(myLetterVariable);
     }
   }
 }
